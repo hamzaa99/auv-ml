98b355846ca16290ade08d5242a766a465f29e94
Sofian Khalfi <sofian.khalfi@myexpat.fr>
2022-04-06 17:43:41 +0200

    scraping but blacklist

--- AdopteUneVille/spiders/villeideale_spider.py
+++ AdopteUneVille/spiders/villeideale_spider.py
@@ -1,4 +1,6 @@
 import scrapy
+from scrapy import Selector
+
 from AdopteUneVille import recupurl
 
 class VilleidealeSpider(scrapy.Spider):
@@ -6,12 +8,25 @@ class VilleidealeSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = recupurl.constructurls()
+        #urls = ['https://www.ville-ideale.fr/aix-les-bains_73008',]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+
+        infos = response.xpath('/html/body/div/div/table[@id="tablonotes"]/tr[2]').extract()
+        self.log(infos)
+        yield {
+            'Nom' : response.request.url,
+            'Environnement': response.xpath('/html/body/div/div/table[@id="tablonotes"]/tr[1]/td/text()').extract_first(),
+            'Transports' : response.xpath('/html/body/div/div/table[@id="tablonotes"]/tr[2]/td/text()').extract_first(),
+            'Sécurité': response.xpath('/html/body/div/div/table[@id="tablonotes"]/tr[3]/td/text()').extract_first(),
+            'Santé': response.xpath('/html/body/div/div/table[@id="tablonotes"]/tr[4]/td/text()').extract_first(),
+            'SportsEtLoisirs': response.xpath('/html/body/div/div/table[@id="tablonotes"]/tr[5]/td/text()').extract_first(),
+            'Culture': response.xpath('/html/body/div/div/table[@id="tablonotes"]/tr[6]/td/text()').extract_first(),
+            'Enseignement': response.xpath('/html/body/div/div/table[@id="tablonotes"]/tr[4]/td/text()').extract_first(),
+            'Commerce': response.xpath('/html/body/div/div/table[@id="tablonotes"]/tr[4]/td/text()').extract_first(),
+            'QualiteDeVie': response.xpath('/html/body/div/div/table[@id="tablonotes"]/tr[4]/td/text()').extract_first(),
+        }
+
+
