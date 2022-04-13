886bebd458274a9004361f13ad8a49aa731a303f
Sofian Khalfi <sofian.khalfi@myexpat.fr>
2022-04-13 11:46:09 +0200

    correction bug

--- machine_learning/dbscan.py
+++ machine_learning/dbscan.py
@@ -30,4 +30,37 @@ lyon1 = villes.loc['69381']
 lyon2 = villes.loc['69382']
 chatillon = villes.loc['92020']
 
-print(db.core_sample_indices_)
+import matplotlib.pyplot as plt
+
+# Black removed and is used for noise instead.
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 1]
+
+    class_member_mask = labels == k
+
+    xy = villes[class_member_mask & core_samples_mask]
+    plt.plot(
+        xy[:, 0],
+        xy[:, 1],
+        "o",
+        markerfacecolor=tuple(col),
+        markeredgecolor="k",
+        markersize=14,
+    )
+
+    xy = villes[class_member_mask & ~core_samples_mask]
+    plt.plot(
+        xy[:, 0],
+        xy[:, 1],
+        "o",
+        markerfacecolor=tuple(col),
+        markeredgecolor="k",
+        markersize=6,
+    )
+
+plt.title("Estimated number of clusters: %d" % n_clusters_)
+plt.show()
