4408d5812748dec1d64521baf2b1065613c30f10
Sofian Khalfi <sofian.khalfi@myexpat.fr>
2022-04-13 11:09:49 +0200

    dbscan et csv merge

--- machine_learning/dbscan.py
+++ machine_learning/dbscan.py
@@ -1,13 +1,7 @@
 import pandas as pd
-import pickle as pickle
-from numpy import unique
-from numpy import where
-from sklearn.datasets import make_classification
 from sklearn.cluster import DBSCAN
-from matplotlib import pyplot
 import numpy as np
 from sklearn import metrics
-
 
 ## dossier prenant en compte uniquement les données liées au commerce
 villes = pd.read_csv('../data/merged/test.csv',delimiter=",",header=0,index_col='CODGEO', dtype={'CODGEO': 'str'})
@@ -18,7 +12,6 @@ db = DBSCAN(eps=0.3, min_samples=10).fit(villes)
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
 labels = db.labels_
-labels_true = villes
 
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 n_noise_ = list(labels).count(-1)
@@ -26,12 +19,15 @@ n_noise_ = list(labels).count(-1)
 
 print("Estimated number of clusters: %d" % n_clusters_)
 print("Estimated number of noise points: %d" % n_noise_)
-print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
-print(
-    "Adjusted Mutual Information: %0.3f"
-    % metrics.adjusted_mutual_info_score(labels_true, labels)
-)
-print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
+print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(villes, labels))
+
+drancy = villes.loc['93029']
+laCourneuve = villes.loc['93027']
+annecy = villes.loc['74010']
+sceaux = villes.loc['92071']
+antony = villes.loc['92002']
+lyon1 = villes.loc['69381']
+lyon2 = villes.loc['69382']
+chatillon = villes.loc['92020']
+
+print(db.core_sample_indices_)
